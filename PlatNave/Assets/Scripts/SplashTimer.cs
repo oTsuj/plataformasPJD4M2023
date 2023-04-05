260f5a0ec32de0829321adbdae06678b49a7a8ef
Justo Fernandes de Oliveira Neto <justo.neto@academico.ifrn.edu.br>
2023-04-05 12:03:01 -0300

    d2

---
+++ PlatNave/Assets/Scripts/SplashTimer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashTimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(2f);
+        GameManager.Instance.LoadScene("MainMenu");
+    }
+
+    
+}
